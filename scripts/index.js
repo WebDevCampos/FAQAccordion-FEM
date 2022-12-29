f67f1554bea731f2338a1fabcd5d552b63ec2557
Marcus Rangel <webdevcampos@gmail.com>
2022-12-29 15:56:00 -0300

    Almost finished

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,30 @@
-const details = document.querySelectorAll("details");
+const questions = document.querySelectorAll(".question");
+const answers = document.querySelectorAll(".answer");
+const questionTitles = document.querySelectorAll(".question-title");
+questions.forEach((question, index) => {
+  questionTitles[1].querySelector("h4").style.fontWeight = "bold";
+  question.addEventListener("click", (e) => {
+    answers.forEach((answer, i) => {
+      index == i
+        ? answer.classList.add("show")
+        : answer.classList.remove("show");
+    });
+  });
+});
+questionTitles.forEach((title) => {
+  title.addEventListener("click", (e) => {
+    questionTitles.forEach((h4) => {
+      h4.querySelector("h4").style.fontWeight = "lighter";
+    });
 
-details.forEach((detail) => {
-  console.log(detail);
+    questionTitles.forEach((image) => {
+      image
+        .querySelector("img")
+        .setAttribute("src", "images/icon-arrow-down.svg");
+    });
+
+    title.querySelector("h4").style.fontWeight = "bold";
+
+    title.querySelector("img").setAttribute("src", "images/icon-arrow-up.svg");
+  });
 });
